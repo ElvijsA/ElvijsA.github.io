4263149553fc8d5d66b584720d63dff58efc37c9
= <eandrusjaks@gmail.com>
2017-11-05 20:08:46 +0000

    async js scripts

--- assets/js/contact-validation.js
+++ assets/js/contact-validation.js
@@ -1,10 +1,11 @@
-$(function() {
+$(window).load(function() {
   var error = false;
   function check_name() {
     var name_lenght = $('#contact-name').val().length;
 
     if(name_lenght < 3 || name_lenght > 20) {
       $('#contact-name').addClass('is-danger');
+      $('.name-error').text('Name must be 3 to 20 characters long')
       error = true;
     }else{
       $('#contact-name').removeClass('is-danger');
@@ -15,6 +16,7 @@ $(function() {
     var message_lenght = $('#contact-message').val().length;
 
     if(message_lenght < 10 || message_lenght > 200) {
+      $('.message-error').text('Message must be 10 to 200 characters long!')
       $('#contact-message').addClass('is-danger');
       error = true;
     }else{
@@ -28,6 +30,7 @@ $(function() {
     if(pattern.test($('#contact-email').val())){
       $('#contact-email').removeClass('is-danger');
     }else{
+      $('.email-error').text('Please Input Valid Email Adress')
       $('#contact-email').addClass('is-danger');
       error = true;
     }
